ba99dc6df51604b40f89a0ff118d1ebfb7412bf5
Denis O <denis@universal-development.com>
2017-05-27 21:30:31 +0300

    Added basic operations on tag index storage

--- polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagIndexStorage.java
+++ polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagIndexStorage.java
@@ -2,6 +2,8 @@ package com.unidev.polydata;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.unidev.polydata.domain.BasicPoly;
+import java.util.Optional;
 import org.bson.Document;
 
 /**
@@ -15,9 +17,29 @@ public class TagIndexStorage extends AbstractPolyStorage {
     super(mongoClient, mongoDatabase);
   }
 
-  public MongoCollection<Document> fetchTagIndexCollection(String poly, String tagIndex) {
+  public MongoCollection<Document> fetchCollection(String poly, String tagIndex) {
     return mongoClient.getDatabase(mongoDatabase)
         .getCollection(poly + "." + TAG_INDEX_COLLECTION + "." + tagIndex);
+  }
+
+  public boolean exist(String poly, String tagIndex, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly, tagIndex);
+    return exist(collection, id);
+  }
+
+  public BasicPoly save(String poly, String tagIndex, BasicPoly basicPoly) {
+    MongoCollection<Document> collection = fetchCollection(poly, tagIndex);
+    return save(collection, basicPoly);
+  }
+
+  public Optional<BasicPoly> fetchPoly(String poly, String tagIndex, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly, tagIndex);
+    return fetchPoly(collection, id);
+  }
+
+  public boolean removePoly(String poly, String tagIndex, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly, tagIndex);
+    return removePoly(collection, id);
   }
 
 
--- polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagStorage.java
+++ polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagStorage.java
@@ -2,6 +2,8 @@ package com.unidev.polydata;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.unidev.polydata.domain.BasicPoly;
+import java.util.Optional;
 import org.bson.Document;
 
 /**
@@ -15,9 +17,28 @@ public class TagStorage extends AbstractPolyStorage {
     super(mongoClient, mongoDatabase);
   }
 
-  private MongoCollection<Document> fetchTagsCollection(String poly) {
+  private MongoCollection<Document> fetchCollection(String poly) {
     return mongoClient.getDatabase(mongoDatabase).getCollection(poly + "." + TAGS_COLLECTION);
   }
 
+  public boolean exist(String poly, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly);
+    return exist(collection, id);
+  }
+
+  public BasicPoly save(String poly, BasicPoly basicPoly) {
+    MongoCollection<Document> collection = fetchCollection(poly);
+    return save(collection, basicPoly);
+  }
+
+  public Optional<BasicPoly> fetchPoly(String poly, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly);
+    return fetchPoly(collection, id);
+  }
+
+  public boolean removePoly(String poly, String id) {
+    MongoCollection<Document> collection = fetchCollection(poly);
+    return removePoly(collection, id);
+  }
 
 }
