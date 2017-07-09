bafea9065eb984542f5dbe0e55f2379a069c4bb1
Denis O <denis@universal-development.com>
2017-07-09 11:50:40 +0300

    Added not found exception code

--- polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/api/PolyNotFoundException.java
+++ polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/api/PolyNotFoundException.java
@@ -1,6 +1,9 @@
 package com.unidev.polydata.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PolyNotFoundException extends RuntimeException {
 
   public PolyNotFoundException() {
