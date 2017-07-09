8e13a10f31321c947751b6681974410b215e3d43
Denis O <denis@universal-development.com>
2017-07-09 11:57:30 +0300

    Added usage of lombok

--- polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/api/Query.java
+++ polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/api/Query.java
@@ -1,6 +1,15 @@
 package com.unidev.polydata.api;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Query {
 
   private int offset;
