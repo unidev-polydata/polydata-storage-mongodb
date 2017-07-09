16efe2b9958e2442a2a8c8513bb59b3d1b00f56d
Denis O <denis@universal-development.com>
2017-07-09 14:46:23 +0300

    Removed jminix dependency

--- polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/Application.java
+++ polydata-storage-mongodb-rest-api/src/main/java/com/unidev/polydata/Application.java
@@ -3,13 +3,9 @@ package com.unidev.polydata;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.unidev.platform.j2ee.common.WebUtils;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import org.jminix.console.servlet.MiniConsoleServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class Application implements ServletContextInitializer {
+public class Application {
 
   @Value("${polydata.mongodb}")
   private String polydataMongodbUri;
@@ -31,12 +27,6 @@ public class Application implements ServletContextInitializer {
   @Bean
   public WebUtils webUtils() {
     return new WebUtils();
-  }
-
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-    servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class)
-        .addMapping("/jmx/*");
   }
 
   @Bean
