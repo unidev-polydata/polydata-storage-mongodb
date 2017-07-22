5db55b8b2f86afc5efd0f949d03af36b2bc97c51
Denis O <denis@universal-development.com>
2017-07-23 00:28:29 +0300

    Custom tags storage

--- polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagStorage.java
+++ polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/TagStorage.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.Document;
 
 /**
@@ -22,117 +23,170 @@ import org.bson.Document;
  */
 public class TagStorage extends AbstractPolyStorage {
 
-  public TagStorage(MongoClient mongoClient, String mongoDatabase) {
-    super(mongoClient, mongoDatabase);
-  }
-
-  protected void migrate(String poly) {
-    MongoChangesResultStorage mongoChangesResultStorage = new MongoChangesResultStorage(mongoClient,
-        mongoDatabase, poly + "." + TAGS_COLLECTION + ".changes");
-    ChangesCore changesCore = new ChangesCore(mongoChangesResultStorage);
-    changesCore.addChange(new TagsCountIndex());
-
-    ChangeContext changeContext = new ChangeContext();
-    changeContext.put(MongodbChange.MONGO_CLIENT_KEY, mongoClient);
-    changeContext.put(MongodbChange.DATABASE_KEY, mongoDatabase);
-    changeContext.put(MongodbChange.COLLECTION_KEY, fetchCollection(poly));
-
-    changesCore.executeChanges(changeContext);
-
-  }
-
-  public String fetchCollectionName(String poly) {
-    return poly + "." + TAGS_COLLECTION;
-  }
-
-  public MongoCollection<Document> fetchCollection(String poly) {
-    return mongoClient.getDatabase(mongoDatabase).getCollection(fetchCollectionName(poly));
-  }
-
-  public List<BasicPoly> listTags(String poly) {
-    MongoCollection<Document> documentMongoCollection = fetchCollection(poly);
-    List<BasicPoly> tags = new ArrayList<>();
-    documentMongoCollection.find().iterator().forEachRemaining(document -> {
-      BasicPoly tag = new BasicPoly();
-      tag.putAll(document);
-      tags.add(tag);
-    });
-
-    return tags;
-  }
-
-  /**
-   * Save and increment tag count
-   *
-   * @return Saved poly tag
-   */
-  public BasicPoly addTag(String poly, BasicPoly tag) {
-    if (!exist(poly, tag._id())) {
-      tag.put(COUNT_KEY, 1);
-      return save(poly, tag);
-    }
-    BasicPoly storedTag = fetchPoly(poly, tag._id()).get();
-    int count = storedTag.fetch(COUNT_KEY, 1);
-    storedTag.putAll(tag);
-    storedTag.put(COUNT_KEY, count + 1);
-    return update(poly, storedTag);
-  }
-
-  public void addTag(String poly, Collection<BasicPoly> tags) {
-    tags.forEach(tag -> addTag(poly, tag));
-  }
-
-  /**
-   * Remove tag from counting, decrementing tag count, if count reaches 0 - remove poly.
-   *
-   * @return Updated poly tag
-   */
-  public Optional<BasicPoly> removeTag(String poly, String id) {
-    if (!exist(poly, id)) {
-      return Optional.empty();
+    public TagStorage(MongoClient mongoClient, String mongoDatabase) {
+        super(mongoClient, mongoDatabase);
     }
 
-    BasicPoly storedTag = fetchPoly(poly, id).get();
-    int count = storedTag.fetch(COUNT_KEY, 1);
-    count--;
-    storedTag.put(COUNT_KEY, count);
-    if (count == 0) {
-      removePoly(poly, id);
-      return Optional.of(storedTag);
+    protected void migrate(String poly, String storage) {
+        String suffix = "";
+        if (StringUtils.isNotBlank(suffix)) {
+            suffix = "." + storage;
+        }
+        MongoChangesResultStorage mongoChangesResultStorage = new MongoChangesResultStorage(
+            mongoClient,
+            mongoDatabase, poly + "." + TAGS_COLLECTION + suffix + ".changes");
+        ChangesCore changesCore = new ChangesCore(mongoChangesResultStorage);
+        changesCore.addChange(new TagsCountIndex());
+
+        ChangeContext changeContext = new ChangeContext();
+        changeContext.put(MongodbChange.MONGO_CLIENT_KEY, mongoClient);
+        changeContext.put(MongodbChange.DATABASE_KEY, mongoDatabase);
+        changeContext.put(MongodbChange.COLLECTION_KEY, fetchCollection(poly));
+
+        changesCore.executeChanges(changeContext);
+
     }
-    return Optional.of(update(poly, storedTag));
 
-  }
+    @Deprecated
+    protected void migrate(String poly) {
+        migrate(poly, null);
+    }
 
-  public void removeTag(String poly, Collection<BasicPoly> tags) {
-    tags.stream().map(tag -> tag._id()).forEach(id -> removeTag(poly, id));
-  }
+    @Deprecated
+    public String fetchCollectionName(String poly) {
+        return fetchCollectionName(poly, null);
+    }
 
-  public boolean exist(String poly, String id) {
-    MongoCollection<Document> collection = fetchCollection(poly);
-    return exist(collection, id);
-  }
+    public String fetchCollectionName(String poly, String storage) {
+        String suffix = "";
+        if (StringUtils.isNotBlank(suffix)) {
+            suffix = "." + storage;
+        }
+        return poly + "." + TAGS_COLLECTION + suffix;
+    }
 
-  protected BasicPoly save(String poly, BasicPoly basicPoly) {
-    MongoCollection<Document> collection = fetchCollection(poly);
-    return save(collection, basicPoly);
-  }
 
-  protected BasicPoly update(String poly, BasicPoly basicPoly) {
-    MongoCollection<Document> collection = fetchCollection(poly);
-    return update(collection, basicPoly);
-  }
+    public MongoCollection<Document> fetchCollection(String poly) {
+        return mongoClient.getDatabase(mongoDatabase).getCollection(fetchCollectionName(poly));
+    }
 
-  public Optional<BasicPoly> fetchPoly(String poly, String id) {
-    MongoCollection<Document> collection = fetchCollection(poly);
-    return fetchPoly(collection, id);
-  }
+    public MongoCollection<Document> fetchCollection(String poly, String storage) {
+        return mongoClient.getDatabase(mongoDatabase)
+            .getCollection(fetchCollectionName(poly, storage));
+    }
 
-  public boolean removePoly(String poly, String id) {
-    MongoCollection<Document> collection = fetchCollection(poly);
-    return removePoly(collection, id);
-  }
+    @Deprecated
+    public List<BasicPoly> listTags(String poly) {
+        return listTags(poly, null);
+    }
 
+    public List<BasicPoly> listTags(String poly, String storage) {
+        MongoCollection<Document> documentMongoCollection = fetchCollection(poly, storage);
+        List<BasicPoly> tags = new ArrayList<>();
+        documentMongoCollection.find().iterator().forEachRemaining(document -> {
+            BasicPoly tag = new BasicPoly();
+            tag.putAll(document);
+            tags.add(tag);
+        });
+
+        return tags;
+    }
+
+    @Deprecated
+    public BasicPoly addTag(String poly, BasicPoly tag) {
+        return addTag(poly, null, tag);
+    }
+
+    /**
+     * Save and increment tag count
+     *
+     * @return Saved poly tag
+     */
+    public BasicPoly addTag(String poly, String storage, BasicPoly tag) {
+        if (!exist(poly, storage, tag._id())) {
+            tag.put(COUNT_KEY, 1);
+            return save(poly, tag);
+        }
+        BasicPoly storedTag = fetchPoly(poly, storage, tag._id()).get();
+        int count = storedTag.fetch(COUNT_KEY, 1);
+        storedTag.putAll(tag);
+        storedTag.put(COUNT_KEY, count + 1);
+        return update(poly, storedTag);
+    }
+
+    @Deprecated
+    public void addTag(String poly, Collection<BasicPoly> tags) {
+        tags.forEach(tag -> addTag(poly, tag));
+    }
+
+    public void addTag(String poly, String storage, Collection<BasicPoly> tags) {
+        tags.forEach(tag -> addTag(poly, storage, tag));
+    }
+
+    @Deprecated
+    public Optional<BasicPoly> removeTag(String poly, String id) {
+        return removeTag(poly, null, id);
+    }
+
+    /**
+     * Remove tag from counting, decrementing tag count, if count reaches 0 - remove poly.
+     *
+     * @return Updated poly tag
+     */
+    public Optional<BasicPoly> removeTag(String poly, String storage, String id) {
+        if (!exist(poly, storage, id)) {
+            return Optional.empty();
+        }
+
+        BasicPoly storedTag = fetchPoly(poly, storage, id).get();
+        int count = storedTag.fetch(COUNT_KEY, 1);
+        count--;
+        storedTag.put(COUNT_KEY, count);
+        if (count == 0) {
+            removePoly(poly, storage, id);
+            return Optional.of(storedTag);
+        }
+        return Optional.of(update(poly, storedTag));
+
+    }
+
+    @Deprecated
+    public void removeTag(String poly, Collection<BasicPoly> tags) {
+        tags.stream().map(tag -> tag._id()).forEach(id -> removeTag(poly, null, id));
+    }
+
+    public void removeTag(String poly, String storage, Collection<BasicPoly> tags) {
+        tags.stream().map(tag -> tag._id()).forEach(id -> removeTag(poly, storage, id));
+    }
+
+    public boolean exist(String poly, String storage, String id) {
+        MongoCollection<Document> collection = fetchCollection(poly, storage);
+        return exist(collection, id);
+    }
+
+    protected BasicPoly save(String poly, BasicPoly basicPoly) {
+        MongoCollection<Document> collection = fetchCollection(poly);
+        return save(collection, basicPoly);
+    }
+
+    protected BasicPoly update(String poly, BasicPoly basicPoly) {
+        MongoCollection<Document> collection = fetchCollection(poly);
+        return update(collection, basicPoly);
+    }
+
+    public Optional<BasicPoly> fetchPoly(String poly, String id) {
+        return fetchPoly(poly, null, id);
+    }
+
+    public Optional<BasicPoly> fetchPoly(String poly, String storage, String id) {
+        MongoCollection<Document> collection = fetchCollection(poly, storage);
+        return fetchPoly(collection, id);
+    }
+
+    public boolean removePoly(String poly, String storage, String id) {
+        MongoCollection<Document> collection = fetchCollection(poly, storage);
+        return removePoly(collection, id);
+    }
 
 
 }
