20b98eaa170dccd6c5950781ebf10b68fbc57cac
Denis O <denis@universal-development.com>
2017-09-17 19:49:39 +0300

    POLY-143 Added poly constructor

--- polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/PolyRecord.java
+++ polydata-storage-mongodb-core/src/main/java/com/unidev/polydata/PolyRecord.java
@@ -5,6 +5,7 @@ import static com.unidev.polydata.MongodbStorage.TAGS_KEY;
 import com.unidev.polydata.domain.BasicPoly;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Map;
 import org.bson.Document;
 
 /**
@@ -12,52 +13,57 @@ import org.bson.Document;
  */
 public class PolyRecord extends BasicPoly {
 
-  public PolyRecord() {
-  }
-
-  public PolyRecord(Document document) {
-    super.putAll(document);
-    populateTags();
-  }
-
-  /**
-   * Populate tags fields
-   */
-  private void populateTags() {
-    Collection<Document> rawTags = fetch(TAGS_KEY);
-    if (rawTags != null) {
-      Collection<BasicPoly> tags = new HashSet<>();
-      rawTags.forEach(rawTag -> {
-        BasicPoly basicPoly = new BasicPoly();
-        basicPoly.putAll(rawTag);
-        tags.add(basicPoly);
-      });
-      put(TAGS_KEY, tags);
+    public PolyRecord() {
     }
-  }
 
-  public Collection<BasicPoly> fetchTags() {
-    return fetch(TAGS_KEY);
-  }
+    public PolyRecord(Map map) {
+        super.putAll(map);
+        populateTags();
+    }
 
-  public void addTag(BasicPoly basicPoly) {
-    if (!containsKey(TAGS_KEY)) {
-      put(TAGS_KEY, new HashSet<BasicPoly>());
+    public PolyRecord(Document document) {
+        super.putAll(document);
+        populateTags();
     }
-    fetchTags().add(basicPoly);
-  }
 
-  public void removeTag(String id) {
-    BasicPoly searchedPoly = null;
-    for (BasicPoly poly : fetchTags()) {
-      if (id.equalsIgnoreCase(poly._id())) {
-        searchedPoly = poly;
-        break;
-      }
+    /**
+     * Populate tags fields
+     */
+    private void populateTags() {
+        Collection<Document> rawTags = fetch(TAGS_KEY);
+        if (rawTags != null) {
+            Collection<BasicPoly> tags = new HashSet<>();
+            rawTags.forEach(rawTag -> {
+                BasicPoly basicPoly = new BasicPoly();
+                basicPoly.putAll(rawTag);
+                tags.add(basicPoly);
+            });
+            put(TAGS_KEY, tags);
+        }
     }
-    if (searchedPoly != null) {
-      fetchTags().remove(searchedPoly);
+
+    public Collection<BasicPoly> fetchTags() {
+        return fetch(TAGS_KEY);
     }
-  }
+
+    public void addTag(BasicPoly basicPoly) {
+        if (!containsKey(TAGS_KEY)) {
+            put(TAGS_KEY, new HashSet<BasicPoly>());
+        }
+        fetchTags().add(basicPoly);
+    }
+
+    public void removeTag(String id) {
+        BasicPoly searchedPoly = null;
+        for (BasicPoly poly : fetchTags()) {
+            if (id.equalsIgnoreCase(poly._id())) {
+                searchedPoly = poly;
+                break;
+            }
+        }
+        if (searchedPoly != null) {
+            fetchTags().remove(searchedPoly);
+        }
+    }
 
 }
